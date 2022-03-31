9d736608ff45f4ffceb853c9daccdd9b5610a217
Lucas <17826152702@163.com>
2022-04-01 00:12:52 +0800

    20220331 commit

---
+++ src/LeetCodeOffer21Easy.java
@@ -0,0 +1,19 @@
+public class LeetCodeOffer21Easy {
+    class Solution {
+        public int[] exchange(int[] nums) {
+            int n = nums.length;
+            //left指向奇数的下一个位置
+            //right指针一直往后移动直到末尾
+            int left = 0;
+            for (int right = 0; right < n; right++) {
+                if (nums[right] % 2 == 1) {
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                    ++left;
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ src/LeetCodeOffer57Easy.java
@@ -0,0 +1,18 @@
+public class LeetCodeOffer57Easy {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int left = 0, right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    return new int[]{nums[left], nums[right]};
+                } else if (nums[left] + nums[right] < target) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+            return new int[0];
+        }
+    }
+}
