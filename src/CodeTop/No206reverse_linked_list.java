d2561be60eb138f01170a7ad7efa138a4212fd30
Lucas <17826152702@163.com>
2022-07-04 23:30:00 +0800

    codetop 206

---
+++ src/CodeTop/No206reverse_linked_list.java
@@ -0,0 +1,52 @@
+package CodeTop;
+
+public class No206reverse_linked_list {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public class ListNode {
+            int val;
+            ListNode next;
+            ListNode() {}
+            ListNode(int val) { this.val = val; }
+            ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
+        //使用额外空间
+//        public ListNode reverseList(ListNode head) {
+//            if (head == null) return null;
+//            Deque<ListNode> deque = new LinkedList<>();
+//            while (head != null) {
+//                deque.push(head);
+//                head = head.next;
+//            }
+//            ListNode temp = new ListNode();
+//            temp.next = deque.peek();
+//            while (deque.size() > 0) {
+//                ListNode cur = deque.pop();
+//                cur.next = deque.peek();
+//            }
+//            return temp.next;
+//        }
+
+        //双指针
+        public ListNode reverseList(ListNode head) {
+            if (head == null) return null;
+            ListNode pre = null, cur = head, temp;
+            while (cur != null) {
+                temp = cur.next;
+                cur.next = pre;
+                cur = temp;
+                pre = cur;
+            }
+            return pre;
+        }
+    }
+}
